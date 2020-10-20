a98fbb096c0e20dfdb52c80a2777b12e4e717b93
RMKendall <ryan.morris.kendall@gmail.com>
2020-10-20 14:04:03 -0500

    Complete initial Dependencies and Maven exercise
    - Added exclude file to ignore classes and target files
    - Added Properties block to POM to correct Java 5 error during build

--- src/main/java/StringPlay.java
+++ src/main/java/StringPlay.java
@@ -1,7 +1,13 @@
+import org.apache.commons.lang3.StringUtils;
+
 public class StringPlay {
     public static void main(String[] args) {
         Input input = new Input();
         System.out.println("Please enter a number: ");
-
+        System.out.println(StringUtils.isNumeric(input.getString()));
+        System.out.println("Please enter some words: ");
+        System.out.println(StringUtils.swapCase(input.getString()));
+        System.out.println("Another phrase please: ");
+        System.out.println(StringUtils.reverse(input.getString()));
     }
 }
